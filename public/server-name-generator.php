6c7b72ba3943eb51336e0ad00d9e86c262a48184
ciaorachel <ciaorachel@gmail.com>
2015-07-20 15:26:14 -0500

    cleaned up coding - made function more reusable, played with CSS

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,35 +1,45 @@
 <?php
 
-$adjectives = ['Massive', 'Titanic', 'Monster', 'Jumbo', 'Mighty', 'Brawny', 'Stout', 'Mega', 'King-Size', 'Hefty'];
-$nouns = ['Rose', 'Violet', 'Bluebonnet', 'Orchid', 'Daisy', 'Marigold', 'Clover', 'Dandelion', 'Lilac', 'Pansy'];
+$adjectives = array('Massive', 'Titanic', 'Monster', 'Jumbo', 'Mighty', 'Brawny', 'Stout', 'Beefy', 'Burly', 'Hefty');
+$nouns = array('Rose', 'Violet', 'Bluebonnet', 'Orchid', 'Daisy', 'Marigold', 'Clover', 'Dandelion', 'Lilac', 'Pansy');
 
-//$randAdj = rand(0, 9);
-//$randNoun = rand(0, 9);
-
-//var_dump($adjectives[$randAdj]);
-//var_dump($nouns[$randNoun]);
-
-//var_dump($randAdj);
-//var_dump($randNoun);
-
-function getRandomName ($adjectives, $nouns) {
-	$randAdj = rand(0, 9);
-	$randNoun = rand(0, 9);
-	return "$adjectives[$randAdj] $nouns[$randNoun]" . PHP_EOL;
+function getRandomName ($arrayOne, $arrayTwo) {
+	$randArrayOne = $arrayOne[rand(0, 9)];
+	$randArrayTwo = $arrayTwo[rand(0, 9)];
+	return "$randArrayOne $randArrayTwo";
 }
 
 ?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Server Name Generator</title>
+    <style type="text/css">
+    body {
+    	font-family: Arial, Helvetica, sans-serif;
+    	text-align: center;
+    	background-color: #FFFF66;
+    }
+    h1 {
+    	color: #121212;
+    	font-size: 3rem;
+    }
+    h2 {
+    	color: #333333;
+    	font-size: 3rem;
+    }
+
+	#gradient {
+    	height: 100%;
+    	background-image: linear-gradient(
+    		to right,
+    		#FFFF66, #FF6600
+    		);
+    }
+    </style>
 </head>
-<body>
+<body id="gradient">
 	<h1>Server Name Generator</h1>
 	<h2><?php echo getRandomName($adjectives, $nouns); ?></h2>
-   
-
 </body>
 </html>
