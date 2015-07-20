9a73943987bb8b11370a28b70352bb3a36efdcb9
ciaorachel <ciaorachel@gmail.com>
2015-07-20 15:33:01 -0500

    added some styling, generated array into a table

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -1,18 +1,30 @@
 <?php  
 $myFaveThings = ['kittens', 'puppies', 'bunnies', 'flowers', 'chocolate'];
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>My Favorite Things</title>
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<style>
+	#fancyTable {
+		width: 40%;
+		font-size: 2rem;
+	}
+	</style>
 </head>
 <body>
-	<h1>My Favorite Things</h1>
-	<ol>
-	<?php foreach ($myFaveThings as $myFaveThing) { ?>
-		<li><?php echo $myFaveThing . PHP_EOL; ?></li>
-	<?php } ?>
-	</ol>
+	<div class="container-fluid">
+		<table class="table table-striped" id="fancyTable">
+		<thead><tr><th>
+		My Favorite Things
+		</th></tr></thead>
+
+		<?php foreach ($myFaveThings as $myFaveThing) { ?>
+			<tr><td><?php echo $myFaveThing . PHP_EOL; ?></td></tr>
+		<?php } ?>
+		</table>
+	</div>
+
 </body>
 </html>
