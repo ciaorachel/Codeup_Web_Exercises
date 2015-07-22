c20b011ec62d7ef9e8b047094e77d2b5799dc3dc
ciaorachel <ciaorachel@gmail.com>
2015-07-22 15:36:28 -0500

    added session code to authorized page

--- public/authorized.php
+++ public/authorized.php
@@ -1,20 +1,56 @@
 <?php
-	
-	if ($_POST['username'] == 'guest' && $_POST['password']  == 'password') {
-		$header = "Authorized";
-		$message = "Login successful";
-	} else {
-		if ($_POST['username'] != 'guest' && $_POST['password'] == 'password') { 
-			$header = "NOT Authorized";
-			$message = "Username incorrect";
-		} elseif ($_POST['username'] == 'guest' && $_POST['password'] != 'password') {
-			$header = "NOT Authorized";
-			$message = "Password incorrect";
+
+	session_start();
+
+	if (isset($_GET['reset']) && $_GET['reset'] == 'true') {
+		session_destroy();
+		header('Location: logout.php');
+		exit();
+	} 
+
+	$sessionId = session_id();
+
+	if (!empty($_POST['username']) && !empty($_POST['password'])) {
+		if ($_POST['username'] == 'guest' && $_POST['password']  == 'password') {
+			$header = "Welcome, {$_POST['username']}!";
+			$message = "Login successful";
+			$_SESSION["LOGGED_IN_USER"]= array();
+			$logout = "Logout";
 		} else {
-			$header = "NOT Authorized";
-			$message = "Username and password incorrect";
+			if ($_POST['username'] != 'guest' && $_POST['password'] == 'password') { 
+				$header = "NOT Authorized";
+				$message = "Username incorrect";
+				$logout = "Go back to log in";
+			} elseif ($_POST['username'] == 'guest' && $_POST['password'] != 'password') {
+				$header = "NOT Authorized";
+				$message = "Password incorrect";
+				$logout = "Go back to log in";
+			} else {
+				$header = "NOT Authorized";
+				$message = "Username and password incorrect";
+				$logout = "Go back to log in";
+			}
 		}
+	} else {
+		endSession();
+		header('Location: login.php');
+		exit();
 	}
+
+	function endSession() {
+	    $_SESSION["LOGGED_IN_USER"]= array();
+
+	    if (ini_get("session.use_cookies")) {
+	        $params = session_get_cookie_params();
+	        setcookie(session_name(), '', time() - 42000,
+	            $params["path"], $params["domain"],
+	            $params["secure"], $params["httponly"]
+	        );
+	    }
+
+	    session_destroy();
+	}
+
 
 ?>
 <!DOCTYPE html>
@@ -37,6 +73,8 @@
 <body>
 	<h1><?php echo $header; ?></h1>
 	<h2><?php echo $message; ?></h2>
-	<a href="login.php">Back</a>
+	<a href="logout.php?reset=true"><?php echo $logout; ?></a>
+	<p>Session Id: <?php echo $sessionId; ?></p>
+
 </body>
 </html>
