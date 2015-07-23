d75ddb07b4c681ff11b2e4f44d96404f3208b26b
ciaorachel <ciaorachel@gmail.com>
2015-07-23 08:47:31 -0500

    removed line with session ID from the body -- after much testing, confirmed that new session id is issued at time of login and carries through the session, so this line is no longer needed

---
+++ public/logout2.php
@@ -0,0 +1,25 @@
+<?php  
+	//resume session
+	session_start();
+
+	//destroy the session and redirect to login page
+	endSession();
+	header('Location: login2.php');
+	exit();
+
+	//function to destroy the session
+	function endSession() {
+	    $_SESSION["LOGGED_IN_USER"]= array();
+
+	    if (ini_get("session.use_cookies")) {
+	        $params = session_get_cookie_params();
+	        setcookie(session_name(), '', time() - 42000,
+	            $params["path"], $params["domain"],
+	            $params["secure"], $params["httponly"]
+	        );
+	    }
+
+	    session_destroy();
+	}
+
+?>
