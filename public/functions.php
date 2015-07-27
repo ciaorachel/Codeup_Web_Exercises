76dbb803ff21a00aaba0d564e5de72014e5a5a0a
ciaorachel <ciaorachel@gmail.com>
2015-07-27 10:18:31 -0500

    completed exercise on functions, but for some reason, i overlooked pushing file at the time

---
+++ public/functions.php
@@ -0,0 +1,31 @@
+<?php 
+
+var_dump($_REQUEST);
+
+function inputHas($key) 
+{
+	if (isset($_REQUEST[$key])) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+function inputGet($key)
+{
+	if (isset($_REQUEST[$key])) {
+		return $_REQUEST[$key];
+	} else {
+		return NULL;
+	}
+}
+
+
+function escape($input) 
+{
+	return strip_tags(htmlspecialchars($input));
+}
+
+
+?>
